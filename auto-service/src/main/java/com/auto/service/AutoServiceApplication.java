1fc4b61f820f239e6e3f7c74ebba2c25d0bed059
Miqueas <miqueas.yanotka@gmail.com>
2022-08-23 15:47:40 -0300

    eureka

--- auto-service/src/main/java/com/auto/service/AutoServiceApplication.java
+++ auto-service/src/main/java/com/auto/service/AutoServiceApplication.java
@@ -2,8 +2,10 @@ package com.auto.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AutoServiceApplication {
 
     public static void main(String[] args) {
